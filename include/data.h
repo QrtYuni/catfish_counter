e8d4f2d033297e47b35a762412371e6ad64ba54c
QrtYuni <qrtyuni1303@gmail.com>
2025-02-26 23:10:07 +0700

    make source code for beginning

---
+++ include/data.h
@@ -0,0 +1,9 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#endif
+
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "data.h"
+#include "inference.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+    cout<< "COBAAAAA\n";
+
+}
